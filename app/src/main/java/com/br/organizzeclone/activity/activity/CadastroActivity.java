0a0a837f895aa6fdbf7977b67d060b3cd11a5b22
Antonio Marcos dos Santos <antmarcos@server01.foursys.local>
2021-06-14 10:09:36 -0300

    Autenticando usuario

--- app/src/main/java/com/br/organizzeclone/activity/activity/CadastroActivity.java
+++ app/src/main/java/com/br/organizzeclone/activity/activity/CadastroActivity.java
@@ -1,5 +1,6 @@
 package com.br.organizzeclone.activity.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -9,18 +10,28 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.br.organizzeclone.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.jetbrains.annotations.NotNull;
+
+import config.ConfiguracaoFirebase;
+import model.Usuario;
 
 public class CadastroActivity extends AppCompatActivity {
     private EditText campoNome,campoSenha,campoEmail;
     private Button buttonCadastrar;
-    //private FirebaseAuth autenticacao;
+    private FirebaseAuth autenticacao;
+    private Usuario usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
         inicializaComponentes();
-        // autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
         buttonCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,7 +44,12 @@ public class CadastroActivity extends AppCompatActivity {
                 if(!nome.isEmpty()){
                     if(!email.isEmpty()){
                         if(!senha.isEmpty()){
+                            usuario = new Usuario();
+                            usuario.setNome(nome);
+                            usuario.setEmail(email);
+                            usuario.setSenha(senha);
                             cadastrarUsuario();
+
                         }else{
                             exibirMensagem("Preencha a senha!");
                         }
@@ -49,7 +65,19 @@ public class CadastroActivity extends AppCompatActivity {
     }
 
     public void cadastrarUsuario(){
-        //autenticacao.createUser
+        autenticacao.createUserWithEmailAndPassword(
+                usuario.getEmail(), usuario.getSenha()
+        ).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    exibirMensagem("Cadastro realizado com sucesso!");
+                    finish();
+                }else{
+                    exibirMensagem("Erro ao cadastrar usuário!");
+                }
+            }
+        });
 
     }
     private void exibirMensagem(String texto){
--- app/src/main/java/config/ConfiguracaoFirebase.java
+++ app/src/main/java/config/ConfiguracaoFirebase.java
@@ -1,14 +1,16 @@
 package config;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class ConfiguracaoFirebase {
 
-    //private static FirebaseAuth autenticacao;
+    private static FirebaseAuth autenticacao;
 
     //retorna a instância do FirebaseAuth
-    //public static FirebaseAuth getFirebaseAutenticacao(){
-      //  if(autenticacao == null){
-    //  autenticacao = FirebaseAuth.getInstance();}
-    //    return autenticacao;
-    // }
+    public static FirebaseAuth getFirebaseAutenticacao(){
+        if(autenticacao == null){
+      autenticacao = FirebaseAuth.getInstance();}
+       return autenticacao;
+     }
 
 }
